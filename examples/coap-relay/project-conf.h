a70dfe6b37bdb7c260e1fef74fbb07bdac7c69ff
lovelesh <lovelesh06101990@gmail.com>
2015-12-28 19:33:20 +0530

    changed RF channel to 25

--- examples/coap-relay/project-conf.h
+++ examples/coap-relay/project-conf.h
@@ -42,7 +42,7 @@
 /* Custom channel and PAN ID configuration for your project. */
 
    #undef RF_CHANNEL
-   #define RF_CHANNEL                     26
+   #define RF_CHANNEL                     25
 
    #undef IEEE802154_CONF_PANID
    #define IEEE802154_CONF_PANID          0xABCD
