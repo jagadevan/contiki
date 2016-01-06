a006704e90a7cec180873324dc7ff28e1a623f87
lovelesh <lovelesh06101990@gmail.com>
2016-01-06 14:40:22 +0530

    cleanup after rebase

--- examples/er-rest-example/project-conf.h
+++ examples/er-rest-example/project-conf.h
@@ -40,19 +40,19 @@
 #define __PROJECT_ERBIUM_CONF_H__
 
 /* Custom channel and PAN ID configuration for your project. */
-/*
+
    #undef RF_CHANNEL
-   #define RF_CHANNEL                     26
+   #define RF_CHANNEL                     25
 
    #undef IEEE802154_CONF_PANID
    #define IEEE802154_CONF_PANID          0xABCD
- */
+
 
 /* IP buffer size must match all other hops, in particular the border router. */
-/*
+
    #undef UIP_CONF_BUFFER_SIZE
    #define UIP_CONF_BUFFER_SIZE           256
- */
+
 
 /* Disabling RDC and CSMA for demo purposes. Core updates often
    require more memory. */
@@ -82,10 +82,10 @@
 #define COAP_MAX_OPEN_TRANSACTIONS     4
 
 /* Must be <= open transactions, default is COAP_MAX_OPEN_TRANSACTIONS-1. */
-/*
+
    #undef COAP_MAX_OBSERVERS
    #define COAP_MAX_OBSERVERS             2
- */
+
 
 /* Filtering .well-known/core per query can be disabled to save space. */
 #undef COAP_LINK_FORMAT_FILTERING
